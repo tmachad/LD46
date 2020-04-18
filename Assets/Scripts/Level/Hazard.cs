a9e3d6534ec86a5d97f116846796b83fff02fb10
Tom Machado <tmachad3@gmail.com>
2020-04-18 09:44:22 -0400

    Added on/off time for hazards

--- Assets/Scripts/Level/Hazard.cs
+++ Assets/Scripts/Level/Hazard.cs
@@ -6,6 +6,27 @@ using UnityEngine;
 public class Hazard : MonoBehaviour
 {
     public LayerMask m_PlayerLayer;
+    public float m_OnTime;
+    public float m_OnTimeRandomization;
+    public float m_OffTime;
+    public float m_OffTimeRandomization;
+    private float m_TimeRemaining;
+    private bool m_IsOn;
+
+    private void Update()
+    {
+        m_TimeRemaining -= Time.deltaTime;
+        if (m_TimeRemaining <= 0)
+        {
+            if (m_IsOn)
+            {
+                TurnOff();
+            } else
+            {
+                TurnOn();
+            }
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -23,5 +44,19 @@ public class Hazard : MonoBehaviour
             PlayerController player = collision.GetComponent<PlayerController>();
             player.ChangeHealth(-1);
         }
+    }
+
+    private void TurnOn()
+    {
+        m_IsOn = true;
+        m_TimeRemaining = m_OnTime + Random.Range(0, m_OnTimeRandomization);
+        // Trigger animation for ON state
+    }
+
+    private void TurnOff()
+    {
+        m_IsOn = false;
+        m_TimeRemaining = m_OffTime + Random.Range(0, m_OffTimeRandomization);
+        // Trigger animation for OFF state
     }
 }
