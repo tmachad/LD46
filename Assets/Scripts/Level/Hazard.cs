12dad77b2711a4e0248482f4f0c0fd946f74388d
Tom Machado <tmachad3@gmail.com>
2020-04-18 20:40:48 -0400

    Added some zappy bois along the right stairs

--- Assets/Scripts/Level/Hazard.cs
+++ Assets/Scripts/Level/Hazard.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.Events;
 
 [RequireComponent(typeof(Animator))]
-[RequireComponent(typeof(Collider2D))]
 public class Hazard : MonoBehaviour
 {
     public LayerMask m_PlayerLayer;
