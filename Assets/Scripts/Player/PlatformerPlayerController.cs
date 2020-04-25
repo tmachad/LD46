719261b91e27fba0bd50faf04760b873e1a11f91
Tom Machado <tmachad3@gmail.com>
2020-04-25 12:11:01 -0400

    Added generic PlatformPlayerController

---
+++ Assets/Scripts/Player/PlatformerPlayerController.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlatformerPlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float m_GroundSpeed = 3.0f;
+    public float m_AirDrag = 0.1f;
+    public float m_AirControl = 0.5f;
+    public float m_MaxJumpHeight = 3.0f;
+    public float m_MinJumpHeight = 1.0f;
+    public LayerMask m_GroundLayer;
+    public float m_GroundCheckDistance = 0.05f;
+
+    [Header("Input")]
+    public string m_HorizontalAxis = "Horizontal";
+    public string m_JumpAxis = "Jump";
+    public bool m_IgnoreInputs = false;
+
+    // Component references
+    protected Animator m_Animator;
+    protected Collider2D m_Collider;
+    protected Rigidbody2D m_Rigidbody;
+
+    // State tracking variables
+    private float m_HorizontalInput;
+    private bool m_JumpInput;
+    private bool m_Jumping;
+    private bool m_JumpStillPressed;
+    private bool m_Grounded;
+    
+    protected virtual void Awake()
+    {
+        // Initialize references to components
+        m_Animator = GetComponent<Animator>();
+        m_Collider = GetComponent<Collider2D>();
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    protected virtual void Update()
+    {
+        // Read input and update state
+        if (!m_IgnoreInputs)
+        {
+            m_HorizontalInput = Input.GetAxisRaw(m_HorizontalAxis);
+            m_JumpInput = Input.GetAxisRaw(m_JumpAxis) > 0;
+        } else
+        {
+            m_HorizontalInput = 0;
+            m_JumpInput = false;
+        }
+
+        UpdateAnimator();
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        // Update player's grounded state
+        bool wasGrounded = m_Grounded;
+        m_Grounded = CheckIfGrounded();
+
+        // Update player's movement
+        m_Rigidbody.velocity = UpdateVelocity(m_Rigidbody.velocity, wasGrounded);
+
+        // Update input tracking in FixedUpdate to avoid a scenario where two Updates occur
+        // between FixedUpdates and make it look like the button is being held
+        m_JumpStillPressed = m_JumpInput;
+    }
+
+    protected virtual bool CheckIfGrounded()
+    {
+        Vector2 colliderBottom = new Vector2(m_Collider.bounds.center.x, m_Collider.bounds.min.y);
+        Vector2 checkBoxSize = new Vector2(m_Collider.bounds.size.x * 0.95f, m_GroundCheckDistance);
+        return Physics2D.OverlapBox(colliderBottom, checkBoxSize, 0, m_GroundLayer);
+    }
+
+    protected virtual Vector2 UpdateVelocity(Vector2 velocity, bool wasGrounded)
+    {
+        if (m_HorizontalInput != 0)
+        {
+            velocity.x = m_HorizontalInput * m_GroundSpeed;
+
+            if (!m_Grounded)
+            {
+                // If not grounded use the air control speed
+                velocity.x *= m_AirControl;
+            }
+
+            // Update transform scale to flip player to face the direction they're moving
+            Vector3 scale = transform.localScale;
+            if (m_HorizontalInput < 0)
+            {
+                // Going left, flip to face left
+                scale.x = -1 * Mathf.Abs(scale.x);
+            }
+            else
+            {
+                // Going right, flip to face right
+                scale.x = Mathf.Abs(scale.x);
+            }
+            transform.localScale = scale;
+        }
+        else if (!m_Grounded)
+        {
+            // Not giving input and not grounded, so apply air drag so the player doesn't coast forever
+            velocity.x *= 1 - m_AirDrag;
+        }
+
+        if (m_JumpInput && m_Grounded && !m_JumpStillPressed)
+        {
+            velocity.y = Mathf.Sqrt(-2 * Physics2D.gravity.y * m_Rigidbody.gravityScale * m_MaxJumpHeight);
+            m_Jumping = true;
+        }
+        else if (!m_JumpInput && m_Jumping && velocity.y > 0)
+        {
+            // Stopped holding jump button while jumping and still rising
+            // Apply downward force to converge on m_MinJumpHeight
+            float force = -1 * m_Rigidbody.mass * Physics2D.gravity.y * m_Rigidbody.gravityScale * (1 + m_MaxJumpHeight / m_MinJumpHeight);
+            m_Rigidbody.AddForce(Vector2.down * force, ForceMode2D.Force);
+        }
+
+        if (m_Grounded && !wasGrounded)
+        {
+            // Just hit the ground, so jump must be over
+            m_Jumping = false;
+        }
+
+        return velocity;
+    }
+
+    protected virtual void UpdateAnimator()
+    {
+        m_Animator.SetFloat("Horizontal Speed", Mathf.Abs(m_Rigidbody.velocity.x));
+        m_Animator.SetFloat("Vertical Speed", Mathf.Abs(m_Rigidbody.velocity.y));
+        m_Animator.SetFloat("Horizontal Velocity", m_Rigidbody.velocity.x);
+        m_Animator.SetFloat("Vertical Velocity", m_Rigidbody.velocity.y);
+        m_Animator.SetBool("Grounded", m_Grounded);
+        m_Animator.SetBool("Jumping", m_Jumping);
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,20 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Animator))]
-[RequireComponent(typeof(Rigidbody2D))]
-public class PlayerController : MonoBehaviour { 
-
-    [Header("Movement")]
-    public float m_GroundSpeed = 3.0f;
-    public float m_AirDrag = 0.1f;
-    public float m_MaxJumpHeight = 3.0f;
-    public float m_JumpDownForce = 1.0f;
-
-    [SerializeField]
-    private LayerMask m_GroundLayer;
-    [SerializeField]
-    private float m_GroundCheckDistance = 0.05f;
+public class PlayerController : PlatformerPlayerController { 
 
     [Header("Interaction")]
     [HideInInspector]
@@ -26,129 +13,59 @@ public class PlayerController : MonoBehaviour {
     public int m_MaxHitPoints;
     [SerializeField]
     private int m_HitPoints;
-    public float m_MaxInvulnDuration;
-    private float m_InvulnDuration;
+    public float m_InvulnTime;
     public Vector2 m_OnHitKnockback;
     public float m_StunTime;
-    private float m_StunTimeRemaining;
+    
 
-    private Rigidbody2D m_Rigidbody;
-    private Collider2D m_Collider;
-    private bool m_Grounded = false;
-    private float m_JumpSpeed;
-    private bool m_Jumping;
-    private bool m_JumpStillPressed;
-
+    // Component references
     private ParticleSystem m_ParticleSystem;
-    private Animator m_Animator;
 
-    private void Awake()
+    // State tracking variables
+    private float m_InvulnTimeRemaining;
+    private float m_StunTimeRemaining;
+    private bool m_FixInput;
+    private bool m_Fixing;
+
+    protected override void Awake()
     {
+        base.Awake();
         m_NearbyFixables = new List<Breakable>();
-        m_InvulnDuration = 0;
+        m_InvulnTimeRemaining = 0;
         m_ParticleSystem = GetComponent<ParticleSystem>();
-        m_Animator = GetComponent<Animator>();
     }
 
-    private void Start()
+    protected override void Update()
     {
-        m_Rigidbody = GetComponent<Rigidbody2D>();
-        m_Collider = GetComponent<Collider2D>();
-        m_JumpSpeed = Mathf.Sqrt(-2 * Physics2D.gravity.y * m_Rigidbody.gravityScale * m_MaxJumpHeight);
-    }
-
-    private void Update()
-    {
+        base.Update();
         // Handle fixing things
-        bool fix = Input.GetAxisRaw("Fix") > 0;
+        m_FixInput = Input.GetAxisRaw("Fix") > 0 && !m_IgnoreInputs;
 
-        if (m_StunTimeRemaining > 0)
+
+        if (m_StunTimeRemaining <= 0)
         {
-            fix = false;
-        } else
-        {
-            m_Animator.SetBool("Stunned", false);
+            m_IgnoreInputs = false;
         }
 
-        if (fix && m_NearbyFixables.Count > 0)
+        if (m_FixInput && m_NearbyFixables.Count > 0)
         {
             m_NearbyFixables[0].Fix(m_FixSpeed * Time.deltaTime);
-            m_Animator.SetBool("Fixing", m_NearbyFixables[0].IsBroken());
+            m_Fixing = m_NearbyFixables[0].IsBroken();
         } else
         {
-            m_Animator.SetBool("Fixing", false);
+            m_Fixing = false;
         }
 
-        if (m_InvulnDuration < 0)
-        {
-            m_Animator.SetBool("Invulnerable", false);
-        }
-
-        m_InvulnDuration -= Time.deltaTime;
+        m_InvulnTimeRemaining -= Time.deltaTime;
         m_StunTimeRemaining -= Time.deltaTime;
     }
 
-    private void FixedUpdate()
+    protected override void UpdateAnimator()
     {
-        bool jump = Input.GetAxisRaw("Jump") > 0;
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        Vector2 velocity = new Vector2(m_Rigidbody.velocity.x, m_Rigidbody.velocity.y);
-
-        if (m_StunTimeRemaining > 0)
-        {
-            jump = false;
-            horizontal = 0;
-        }
-
-        // Check for ground below the player
-        Vector2 bottom = new Vector2(transform.position.x + m_Collider.offset.x, transform.position.y + m_Collider.offset.y - m_Collider.bounds.extents.y);
-        Vector2 boxSize = new Vector2(m_Collider.bounds.extents.x * 2 * 0.95f, m_GroundCheckDistance);
-        Collider2D collider = Physics2D.OverlapBox(bottom, boxSize, 0, m_GroundLayer);
-        bool wasGrounded = m_Grounded;
-        m_Grounded = collider != null;
-        m_Animator.SetBool("Grounded", m_Grounded);
-
-
-        // Move horizontally if player is pressing a button, otherwise just maintain current horizontal velocity
-        if (horizontal != 0)
-        {
-            velocity.x = horizontal * m_GroundSpeed;
-
-            if (horizontal < 0)
-            {
-                // Going left, flip sprite to face left
-                transform.localScale = new Vector3(-1 * Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-            } else if (horizontal > 0)
-            {
-                // Going right, flip sprite to face right
-                transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-            }
-        } else if (!m_Grounded)
-        {
-            // When not giving input and not in the air apply air drag so the player doesn't sideways coast forever
-            velocity.x = velocity.x * (1 - m_AirDrag);
-        }
-
-        if (jump && m_Grounded && !m_JumpStillPressed)
-        {
-            // Pressing jump key while grounded, start jumping
-            velocity.y = m_JumpSpeed;
-            m_Jumping = true;
-        } else if (!jump && m_Jumping && m_Rigidbody.velocity.y > 0)
-        {
-            // Stopped holding the jump button while jumping and still rising, apply downward force to shorten jump
-            m_Rigidbody.AddForce(Vector2.down * m_JumpDownForce * m_Rigidbody.mass);
-        }
-
-        if (m_Grounded && !wasGrounded)
-        {
-            // Just hit the ground, so jump must be over
-            m_Jumping = false;
-        }
-
-        m_Rigidbody.velocity = velocity;
-        m_JumpStillPressed = jump;
-        m_Animator.SetFloat("Horizontal Speed", Mathf.Abs(velocity.x));
+        base.UpdateAnimator();
+        m_Animator.SetBool("Stunned", m_StunTimeRemaining > 0);
+        m_Animator.SetBool("Invulnerable", m_InvulnTimeRemaining > 0);
+        m_Animator.SetBool("Fixing", m_Fixing);
     }
 
     /// <summary>
@@ -160,7 +77,7 @@ public class PlayerController : MonoBehaviour {
     {
         if (change < 0)
         {
-            if (m_InvulnDuration > 0)
+            if (m_InvulnTimeRemaining > 0)
             {
                 // Ignore damage when invulnerable
                 return false;
@@ -171,10 +88,8 @@ public class PlayerController : MonoBehaviour {
                 knockback.x *= -Mathf.Sign(m_Rigidbody.velocity.x);
                 m_Rigidbody.velocity = knockback;
                 m_StunTimeRemaining = m_StunTime;
-                m_Animator.SetBool("Stunned", true);
-                m_Animator.SetBool("Invulnerable", true);
-
-                m_InvulnDuration = m_MaxInvulnDuration;
+                m_InvulnTimeRemaining = m_InvulnTime;
+                m_IgnoreInputs = true;
             }
         }
         if (change > 0 && m_HitPoints < m_MaxHitPoints)
